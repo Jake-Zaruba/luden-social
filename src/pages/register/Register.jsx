9cebd97e8cc2b094d8bddb5240b68159e56377d5
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2023-04-21 23:57:00 -0500

    Added registration page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,34 @@
 import "./register.scss";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Luden Social</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          </p>
+          <span>Have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="email" placeholder="Email" />
+            <input type="text" placeholder="Username" />
+            <input
+              type="password"
+              placeholder="Password"
+              autoComplete="new-password"
+            />
+            <input type="text" placeholder="Name" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
