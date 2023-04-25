777c68c9a1c8b7554c6e5fd4f45ea7d02d690483
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2023-04-24 23:41:27 -0500

    Began protecting routes
    Will finish this once I brush up on the new react router v6 methods for protecting routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Layout from "./Layout";
@@ -6,12 +6,25 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 
 function App() {
+  const currentUser = true;
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+  };
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Layout />}>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
           <Route path="home" element={<Home />} />
           <Route path="profile/:id" element={<Profile />} />
         </Route>
