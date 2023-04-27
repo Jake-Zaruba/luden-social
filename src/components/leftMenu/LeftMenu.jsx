5ce03917d4aa213260666cc27a64f1d7ea7fa9e5
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2023-04-26 23:55:40 -0500

    Created homepage left menu

--- src/components/leftMenu/LeftMenu.jsx
+++ src/components/leftMenu/LeftMenu.jsx
@@ -1,7 +1,65 @@
 import "./leftMenu.scss";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import Diversity3Icon from "@mui/icons-material/Diversity3";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import EventAvailableIcon from "@mui/icons-material/EventAvailable";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import MessageIcon from "@mui/icons-material/Message";
+import VideogameAssetIcon from "@mui/icons-material/VideogameAsset";
+import ImageIcon from "@mui/icons-material/Image";
 
 const LeftMenu = () => {
-  return <div>LeftMenu</div>;
+  return (
+    <div className="left-bar">
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/3491688/pexels-photo-3491688.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            <span>Jake Zaruba</span>
+          </div>
+          <div className="item">
+            <PeopleAltIcon className="icon" />
+            <span>Friends</span>
+          </div>
+          <div className="item">
+            <Diversity3Icon className="icon" />
+            <span>Groups</span>
+          </div>
+          <div className="item">
+            <MessageIcon className="icon" />
+            <span>Messages</span>
+          </div>
+          <div className="item">
+            <StorefrontIcon className="icon" />
+            <span>Marketplace</span>
+          </div>
+          <div className="item">
+            <EventAvailableIcon className="icon" />
+            <span>Events</span>
+          </div>
+          <div className="item">
+            <OndemandVideoIcon className="icon" />
+            <span>Videos</span>
+          </div>
+        </div>
+        <hr />
+        <div className="menu">
+          <span>Shortcuts</span>
+          <div className="item">
+            <VideogameAssetIcon className="icon" />
+            <span>Games</span>
+          </div>
+          <div className="item">
+            <ImageIcon className="icon" />
+            <span>Gallery</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftMenu;
